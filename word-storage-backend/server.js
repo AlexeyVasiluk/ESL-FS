0859cc40c1dc2eb9dc70bdab3a7593e9a7533573
Alex <vasiluk.o@gmail.com>
2025-04-22 22:06:45 -0500

    changed mongodb connection

--- word-storage-backend/server.js
+++ word-storage-backend/server.js
@@ -21,7 +21,15 @@ app.use(express.json());
 app.use(cookieParser());
 
 // Підключення до MongoDB
-mongoose.connect(process.env.MONGODB_URI)
+// mongoose.connect(process.env.MONGODB_URI)
+//     .then(() => console.log('Connected to MongoDB'))
+//     .catch((error) => console.error('MongoDB connection error:', error));
+
+mongoose.connect(process.env.MONGODB_URI, {
+    dbName: 'wordStorage',
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
     .then(() => console.log('Connected to MongoDB'))
     .catch((error) => console.error('MongoDB connection error:', error));
 
